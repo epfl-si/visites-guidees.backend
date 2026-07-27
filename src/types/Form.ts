be0e45c24ded105b210b3119b5e2b76d4c41a473
XPL0Z <meneliksigrist2009@gmail.com>
2026-07-27 15:18:01 +0200

    [featur] return language id in /id/getdetails

---
+++ src/types/Form.ts
@@ -0,0 +1,19 @@
+export interface form {
+    placeId: number,
+    firstName: string,
+    lastName: string,
+    company: string,
+    email: string,
+    phone: string,
+    address: string,
+    addressComplement: string,
+    city: string,
+    region: string
+    postalCode: number,
+    country: string,
+    visitDate: number,
+    numberOfParticipants: number,
+    languageId: number,
+    comments: string,
+    gdprConsent: boolean,
+}
