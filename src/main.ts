89cae372f2f19f9ea8cfb6cafa4c5522ca623f9b
XPL0Z <meneliksigrist2009@gmail.com>
2026-08-03 15:35:14 +0200

    [choir] activation of swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WebpackHotModule } from './types/nestjs';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 declare const module: WebpackHotModule;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Guided Tours API')
+    .setDescription('API documentation for the Guided Tours application')
+    .setVersion('1.0')
+    .addTag('cats')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter your Azure AD access token',
+        in: 'header',
+      },
+      'access-token', // this name here is important for matching up with @ApiBearerAuth() decorator
+    )
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   app.enableCors({
     origin: process.env.FRONTEND_URL,
