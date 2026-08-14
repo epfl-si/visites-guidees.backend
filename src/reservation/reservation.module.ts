e9da617a8d241e17e2c9ef4e958a2d2dd2056a28
XPL0Z <meneliksigrist2009@gmail.com>
2026-08-14 15:35:30 +0200

    [feature] implement guide notification

--- src/reservation/reservation.module.ts
+++ src/reservation/reservation.module.ts
@@ -3,9 +3,11 @@ import { ReservationController } from './reservation.controller';
 import { ReservationService } from './reservation.service';
 import { PrismaService } from '../prisma.service';
 import { AzureAdModule } from '../auth/azure-ad.module';
+import { MailModule } from '@/mail/mail.module';
+import { GuideModule } from '../guide/guide.module';
 
 @Module({
-  imports: [AzureAdModule],
+  imports: [AzureAdModule, MailModule, GuideModule],
   controllers: [ReservationController],
   providers: [ReservationService, PrismaService],
 })
